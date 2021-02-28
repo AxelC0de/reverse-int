f5355c9f9730961e655ff50116faacc8a77a2dfc
AxelC0de <AxelC0de.github@gmail.com>
2021-03-01 00:31:23 +0300

    init: code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let nMathabs = Math.abs(n)
+
+	let ntoString = nMathabs.toString()
+
+	let nsplit = ntoString.split('')
+
+	if (nsplit[nsplit.length - 1] == 0) {
+		nsplit.pop()
+	}
+
+	nsplit = nsplit.reverse()
+	nsplit = nsplit.join('')
+
+	Number(nsplit)
+
+	return nsplit
 }
